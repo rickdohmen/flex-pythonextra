f1d22be9e20a41be4a10e4da9b8ee0013a398181
rickd(rickdohmen) <rickd(rickdohmen2003@gmail.com)>
2020-09-24 14:40:11 +0200

    turtle opdrachten pushen

---
+++ 02-filesfolders/huidige_mao.py
@@ -0,0 +1,11 @@
+#hier importeer je de os module
+import os
+
+#de huidige directory opvragen en opslaan in de variable: werkmap
+werkmap = os.getcwd()
+
+#de letters cwd staan voor: current working directory
+
+
+#op het sherm printen
+print("de huidige map is: " + werkmap)
